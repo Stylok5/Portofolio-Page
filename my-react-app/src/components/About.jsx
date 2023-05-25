46756e7ea1d1abec37ef7dde4d7f2d8bdb59b7be
KONSTANTINOS <kostasfergadis74@gmail.com>
2023-05-25 15:51:27 +0300

    last

--- my-react-app/src/components/About.jsx
+++ my-react-app/src/components/About.jsx
@@ -4,15 +4,15 @@ const About = () => {
       <div>
         <h1>About me</h1>
         <p>
-          Hey there! I'm Kostas Fergadis, a junior aspiring Software Developer
-          on the lookout for exciting job opportunities. With a solid foundation
-          in both front-end and back-end technologies, I enjoy crafting
-          efficient and user-friendly full-stack applications. <br /> <br />{" "}
-          With a passion for computers, gaming, and technology since a young
-          age, I naturally gravitated towards the programming world. I soon
-          discovered a profound interest in programming and decided to delve
-          deeper into this fascinating field which led me to a Software
-          Engineering Immersive course at General Assembly.
+          Hey there! I'm Kostas Fergadis, a junior aspiring Software Engineer on
+          the lookout for exciting job opportunities. With a solid foundation in
+          both front-end and back-end technologies, I enjoy crafting efficient
+          and user-friendly full-stack applications. <br /> <br /> With a
+          passion for computers, gaming, and technology since a young age, I
+          naturally gravitated towards the programming world. I soon discovered
+          a profound interest in programming and decided to delve deeper into
+          this fascinating field which led me to a Software Engineering
+          Immersive course at General Assembly.
           {/* <br /> <br /> Eager to expand my knowledge and skills, I'm driven by a
           continuous desire to learn, grow, and contribute in the ever-evolving
           landscape of software development. */}
